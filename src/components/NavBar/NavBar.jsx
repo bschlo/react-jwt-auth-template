a298afac82ce47a1a1dc68524e558532bf5318df
bschloss <benschloss2@gmail.com>
2024-12-13 15:58:07 -0500

    Adding completed front end auth.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,28 @@
-const NavBar = ({ user }) => {
-    return (
-      <>
-        <p>Hello Navbar</p>
-      </>
-    )
-  }
-  export default NavBar
-  
+import { useContext } from 'react';
+import { AuthedUserContext } from '../../App';
+import { Link } from 'react-router-dom';
+
+const NavBar = ({ handleSignout}) => {
+    const user = useContext(AuthedUserContext);
+  return (
+    <>
+      { user ? (
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+          </ul>
+        </nav>
+      ) : (
+        <nav>
+          <ul>
+            <li><Link to="/signin">Sign In</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+          </ul>
+        </nav>
+      )}
+    </>
+  )
+}
+
+export default NavBar;
